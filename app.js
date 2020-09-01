4c670aa3050e9007e381ec59660fd9102a5a6091
thebigbillz <adenugbafavour@gmail.com>
2020-09-01 12:54:11 +0100

    Losing Functionality

--- app.js
+++ app.js
@@ -21,18 +21,38 @@ message.style.display = "none";
 
 //Listen for Guess
 
-guessBtn.addEventListener("click", function () {
+guessBtn.addEventListener("click", function (e) {
   const guess = parseInt(guessInput.value);
 
-  if (!guess || guess < min || guess > max)
+  if (!guess || guess < min || guess > max) {
     setMessage(`Please enter a number between ${min} and ${max}`, "red");
+    return;
+  }
 
   // Check if won
   if (guess === winningNumber) {
     guessInput.disabled = true;
     guessInput.style.borderColor = "green";
     setMessage(`${winningNumber} is correct! YOU WIN!!!`, "green");
+    return;
+  } else {
+    guessesLeft--;
+
+    if (guess !== winningNumber && guessesLeft > 0) {
+      guessInput.style.borderColor = "red";
+      setMessage(
+        `Wrong Guess! | Try Again! | Guesses Left : ${guessesLeft}`,
+        "red"
+      );
+    } else {
+      setMessage(
+        `Wrong Guess! | Game Over! | Correct Answer : ${winningNumber}`,
+        "red"
+      );
+    }
   }
+
+  e.preventDefault();
 });
 
 //Set Message
@@ -43,3 +63,15 @@ function setMessage(msg, color) {
   message.style.backgroundColor = color;
   message.style.border = `1px solid ${color}`;
 }
+
+let x = 4;
+
+function dec(x) {
+  x--;
+  console.log(x);
+}
+
+console.log(x);
+dec(x);
+dec(x);
+console.log(x);
